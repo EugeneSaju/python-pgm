5398e0fa74025cdbeb6517023d67654482f96182
Example <example@abcd.com>
2024-08-26 19:15:04 +0530

    5 commit

--- program.py
+++ program.py
@@ -1,10 +1,18 @@
-import sys
+import os
 
-name = sys.argv[1]
+# Set the environment variable
+env_var_name = "MY_ENV_VAR"
+env_var_value = "HI FROM HWLLO WORLD"
+os.environ[env_var_name] = env_var_value
 
-print(f"Hello, {name}!")
+# Get the environment variable value
+env_var_value = os.environ.get(env_var_name)
 
-if name:
-    print("The secret name is", name)
+# Check if the environment variable exists
+if env_var_value is not None:
+    # Write the environment variable to a text file
+    with open("env_var.txt", "w") as f:
+        f.write(env_var_value)
+    print("Environment variable written to env_var.txt")
 else:
-    print("Name is not found")
+    print("Environment variable not found.")
